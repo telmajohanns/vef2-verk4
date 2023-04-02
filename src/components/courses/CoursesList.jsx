799368df9bca506e573d610971e63542352d6e2c
telmajohanns <tej2@hi.is>
2023-04-02 20:33:34 +0000

    Courses komið, success yay

--- src/components/courses/CoursesList.jsx
+++ src/components/courses/CoursesList.jsx
@@ -1,23 +1,54 @@
-import { useState, useEffect } from 'react';
-//
+import { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
 
-function CInfo(props) {
-    const { slug } = props;
-    const [courses, setCourses] = useState([]);
+// Define the shape of the Course object
+
+function CInfo(_props) {
+  const { slug } = useParams();
+  const [courses, setCourses] = useState([]);
+
   
   useEffect(() => {
-    fetch(`http://localhost:4000/departments/${slug}/courses`)
-    .then((response) => response.json())
-    .then((data) => setCourses(data))
-    .catch((error) => console.error(error));
-}, [slug]);
+    async function fetchData() {
+      try {
+        const response = await fetch(`http://localhost:4000/departments/${slug}/courses`);
+        const data = await response.json();
+        setCourses(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, [slug]);
 
-    return (
-      <div>
-        hey
-      </div>
-    )
-    
+  return (
+    <div>
+      <h1>Departments</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>D</th>
+            <th>Heiti námskeiðis</th>
+            <th>Course units</th>
+            <th>Course semester</th>
+            <th>Course level</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map(course => (
+            <tr key={course.id}>
+              <td>{course.courseId}</td>
+              <td>{course.title}</td>
+              <td>{course.units}</td>
+              <td>{course.semester}</td>
+              <td>{course.level}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
+
 
 export default CInfo;
